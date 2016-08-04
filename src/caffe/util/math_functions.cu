309b403bee7feab0da8095431f4ba4a7405944a7
eriche2016 <1154646635@qq.com>
2016-08-04 12:06:13 +0800

    Update math_functions.cu

--- src/caffe/util/math_functions.cu
+++ src/caffe/util/math_functions.cu
@@ -74,8 +74,10 @@ void caffe_gpu_axpy<double>(const int N, const double alpha, const double* X,
   CUBLAS_CHECK(cublasDaxpy(Caffe::cublas_handle(), N, &alpha, X, 1, Y, 1));
 }
 
+// X: src, Y: des
 void caffe_gpu_memcpy(const size_t N, const void* X, void* Y) {
   if (X != Y) {
+    // copy X to Y
     CUDA_CHECK(cudaMemcpy(Y, X, N, cudaMemcpyDefault));  // NOLINT(caffe/alt_fn)
   }
 }
