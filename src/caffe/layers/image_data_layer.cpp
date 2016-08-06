a7fe99e2ce5bebf801dc21800bc1baaefb0b0114
eriche2016 <1154646635@qq.com>
2016-08-06 11:01:30 +0800

    Update image_data_layer.cpp

--- src/caffe/layers/image_data_layer.cpp
+++ src/caffe/layers/image_data_layer.cpp
@@ -25,21 +25,25 @@ ImageDataLayer<Dtype>::~ImageDataLayer<Dtype>() {
 template <typename Dtype>
 void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
+  // 根据参数文件设置参数
+  // 图像的高度， 宽度， 是否彩色图像，图像目录
   const int new_height = this->layer_param_.image_data_param().new_height();
   const int new_width  = this->layer_param_.image_data_param().new_width();
   const bool is_color  = this->layer_param_.image_data_param().is_color();
   string root_folder = this->layer_param_.image_data_param().root_folder();
-
+  // 当前只支持读取高度和宽度同样大小的图像
   CHECK((new_height == 0 && new_width == 0) ||
       (new_height > 0 && new_width > 0)) << "Current implementation requires "
       "new_height and new_width to be set at the same time.";
   // Read the file with filenames and labels
+  // 读取存放图像名和类标签的列表文件
   const string& source = this->layer_param_.image_data_param().source();
   LOG(INFO) << "Opening file " << source;
   std::ifstream infile(source.c_str());
   string line;
   size_t pos;
   int label;
+  // lines存放文件名和类标的pair
   while (std::getline(infile, line)) {
     pos = line.find_last_of(' ');
     label = atoi(line.substr(pos + 1).c_str());
@@ -48,6 +52,7 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
 
   CHECK(!lines_.empty()) << "File is empty";
 
+  // 是否需要打乱文件的顺序
   if (this->layer_param_.image_data_param().shuffle()) {
     // randomly shuffle data
     LOG(INFO) << "Shuffling data";
@@ -59,6 +64,8 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
 
   lines_id_ = 0;
   // Check if we would need to randomly skip a few data points
+  // 随机跳过的图像，跳过的图像个数在[0, rand_skip-1]之间
+  // 如果rand_skip大于1， 则随机跳过[0, rand_skip-1]个图像
   if (this->layer_param_.image_data_param().rand_skip()) {
     unsigned int skip = caffe_rng_rand() %
         this->layer_param_.image_data_param().rand_skip();
@@ -67,25 +74,32 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
     lines_id_ = skip;
   }
   // Read an image, and use it to initialize the top blob.
+  // 读取文件名到Mat
   cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
                                     new_height, new_width, is_color);
   CHECK(cv_img.data) << "Could not load " << lines_[lines_id_].first;
   // Use data_transformer to infer the expected blob shape from a cv_image.
+  // 对数据的形状进行推断
   vector<int> top_shape = this->data_transformer_->InferBlobShape(cv_img);
+  //设置transformed_data_的形状
   this->transformed_data_.Reshape(top_shape);
   // Reshape prefetch_data and top[0] according to the batch_size.
+  // 设置batch size
   const int batch_size = this->layer_param_.image_data_param().batch_size();
   CHECK_GT(batch_size, 0) << "Positive batch size required";
   top_shape[0] = batch_size;
+  // 设置预取数据中数据的形状
   for (int i = 0; i < this->PREFETCH_COUNT; ++i) {
     this->prefetch_[i].data_.Reshape(top_shape);
   }
+  // 设置输出数据的数据形状
   top[0]->Reshape(top_shape);
 
   LOG(INFO) << "output data size: " << top[0]->num() << ","
       << top[0]->channels() << "," << top[0]->height() << ","
       << top[0]->width();
   // label
+  // 设置预取数据中类标签的形状
   vector<int> label_shape(1, batch_size);
   top[1]->Reshape(label_shape);
   for (int i = 0; i < this->PREFETCH_COUNT; ++i) {
@@ -93,6 +107,7 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
   }
 }
 
+// 打乱图像顺序的数组
 template <typename Dtype>
 void ImageDataLayer<Dtype>::ShuffleImages() {
   caffe::rng_t* prefetch_rng =
@@ -101,6 +116,7 @@ void ImageDataLayer<Dtype>::ShuffleImages() {
 }
 
 // This function is called on prefetch thread
+// 该函数会被内部线程调用
 template <typename Dtype>
 void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   CPUTimer batch_timer;
@@ -111,6 +127,7 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   CHECK(batch->data_.count());
   CHECK(this->transformed_data_.count());
   ImageDataParameter image_data_param = this->layer_param_.image_data_param();
+  // 获取层参数，具体参见层参数的定义的解释
   const int batch_size = image_data_param.batch_size();
   const int new_height = image_data_param.new_height();
   const int new_width = image_data_param.new_width();
@@ -119,6 +136,7 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
 
   // Reshape according to the first image of each batch
   // on single input batches allows for inputs of varying dimension.
+  // 读取跳过之后的第一个第一个图像， 然后进行reshape， 这样允许输入batch内的图像是不同的形状
   cv::Mat cv_img = ReadImageToCVMat(root_folder + lines_[lines_id_].first,
       new_height, new_width, is_color);
   CHECK(cv_img.data) << "Could not load " << lines_[lines_id_].first;
@@ -127,12 +145,14 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   this->transformed_data_.Reshape(top_shape);
   // Reshape batch according to the batch_size.
   top_shape[0] = batch_size;
+  // 对整个batch进行reshape
   batch->data_.Reshape(top_shape);
 
   Dtype* prefetch_data = batch->data_.mutable_cpu_data();
   Dtype* prefetch_label = batch->label_.mutable_cpu_data();
 
   // datum scales
+  // 读取一批图像，并进行预处理
   const int lines_size = lines_.size();
   for (int item_id = 0; item_id < batch_size; ++item_id) {
     // get a blob
@@ -144,18 +164,25 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
     read_time += timer.MicroSeconds();
     timer.Start();
     // Apply transformations (mirror, crop...) to the image
+    // 进行预处理
+    // 根据图像的批次获得图像数据的偏移量
     int offset = batch->data_.offset(item_id);
+    // 设置图像数据的指针到transfromed_data_
     this->transformed_data_.set_cpu_data(prefetch_data + offset);
     this->data_transformer_->Transform(cv_img, &(this->transformed_data_));
+    // 统计预处理时间
     trans_time += timer.MicroSeconds();
-
+    
+    // 复制类标到prefetch_label
     prefetch_label[item_id] = lines_[lines_id_].second;
     // go to the next iter
     lines_id_++;
+    //是否是图像目录中最后一个图像
     if (lines_id_ >= lines_size) {
       // We have reached the end. Restart from the first.
       DLOG(INFO) << "Restarting data prefetching from start.";
       lines_id_ = 0;
+      // 打乱图像索引的顺序
       if (this->layer_param_.image_data_param().shuffle()) {
         ShuffleImages();
       }
@@ -164,6 +191,7 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   batch_timer.Stop();
   DLOG(INFO) << "Prefetch batch: " << batch_timer.MilliSeconds() << " ms.";
   DLOG(INFO) << "     Read time: " << read_time / 1000 << " ms.";
+  // 预处理时间
   DLOG(INFO) << "Transform time: " << trans_time / 1000 << " ms.";
 }
 
